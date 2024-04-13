ec13c988c26bef9dad02014c5874c912301f85f2
Aldiyar <ldr.stlt@gmail.com>
2024-04-14 03:21:00 +0500

    feat: gpt functions

--- app/action.tsx
+++ app/action.tsx
@@ -1,5 +1,5 @@
 import { getStudentPerformanceOverPeriod } from "@/lib/queries";
-import { LineChart } from "@tremor/react";
+import { BarChart, BarList, LineChart, Tracker } from "@tremor/react";
 import { createAI, render } from "ai/rsc";
 import { LoaderIcon } from "lucide-react";
 import OpenAI from "openai";
@@ -8,6 +8,8 @@ import { z } from "zod";
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
+
+const Spinner = () => <LoaderIcon className="animate-spin size-6" />;
 
 async function submitUserMessage(userInput: string) {
   "use server";
@@ -27,7 +29,7 @@ async function submitUserMessage(userInput: string) {
 
     // specific queries
     tools: {
-      get_student_performance_slice: {
+      get_student_performance_timeslice: {
         description: "Get students current performance over a certain period",
         parameters: z
           .object({
@@ -42,7 +44,7 @@ async function submitUserMessage(userInput: string) {
           })
           .required(),
         render: async function* ({ periodStart, periodStop }) {
-          yield <LoaderIcon className="animate-spin size-6" />;
+          yield <Spinner />;
           const data = await getStudentPerformanceOverPeriod(
             periodStart,
             periodStop
@@ -54,6 +56,118 @@ async function submitUserMessage(userInput: string) {
               index={data.index}
               categories={data.cats}
               yAxisWidth={60}
+            />
+          );
+        },
+      },
+      get_student_performance_by_modules: {
+        description:
+          "Get students performance for several modules in one subject. If user asks for their performance on certain subject",
+        parameters: z
+          .object({
+            subjects: z
+              .string()
+              .describe("Subject, which modules need to be shown"),
+          })
+          .required(),
+        render: async function* () {
+          yield <Spinner />;
+          return (
+            <BarList
+              data={[
+                { name: "Module 1", value: 560 },
+                { name: "Module 2", value: 570 },
+                { name: "Module 3", value: 510 },
+              ]}
+              sortOrder="descending"
+              color="green"
+            />
+          );
+        },
+      },
+      get_group_performance_one_module: {
+        description: "Get the performance of student group for one module",
+        parameters: z.object({ module: z.string() }).required(),
+        render: async function* () {
+          yield <Spinner />;
+          return (
+            <BarChart
+              data={[
+                {
+                  name: "Module 3",
+                  Ayazhan: 510,
+                  Assylkhan: 520,
+                  Dimash: 570,
+                  Aqbota: 510,
+                },
+              ]}
+              index="name"
+              categories={["Ayazhan", "Assylkhan", "Dimash", "Aqbota"]}
+              colors={["blue", "teal", "amber", "rose", "indigo", "emerald"]}
+            />
+          );
+        },
+      },
+      get_group_performance_all_modules: {
+        description: "Get the performance of student group for all modules",
+        parameters: z.object({}).required(),
+        render: async function* () {
+          yield <Spinner />;
+          return (
+            <BarChart
+              data={[
+                {
+                  name: "Module 1",
+                  Ayazhan: 560,
+                  Assylkhan: 420,
+                  Dimash: 550,
+                  Aqbota: 410,
+                },
+                {
+                  name: "Module 2",
+                  Ayazhan: 570,
+                  Assylkhan: 500,
+                  Dimash: 500,
+                  Aqbota: 480,
+                },
+                {
+                  name: "Module 3",
+                  Ayazhan: 510,
+                  Assylkhan: 520,
+                  Dimash: 570,
+                  Aqbota: 510,
+                },
+              ]}
+              index="name"
+              categories={["Ayazhan", "Assylkhan", "Dimash", "Aqbota"]}
+              colors={["blue", "teal", "amber", "rose", "indigo", "emerald"]}
+            />
+          );
+        },
+      },
+      get_invdividual_activity_one_module: {
+        description: "Get Inidivual activity or attendance over one module",
+        parameters: z.object({}).required(),
+        render: async function* () {
+          yield <Spinner />;
+          return (
+            <Tracker
+              data={[
+                { color: "emerald", tooltip: "2h" },
+                { color: "emerald", tooltip: "2.5h" },
+                { color: "yellow", tooltip: "1h" },
+                { color: "emerald", tooltip: "2h" },
+                { color: "rose", tooltip: "0h" },
+                { color: "emerald", tooltip: "2h" },
+                { color: "emerald", tooltip: "2h" },
+                { color: "emerald", tooltip: "2h" },
+                { color: "emerald", tooltip: "2h" },
+                { color: "emerald", tooltip: "2h" },
+                { color: "emerald", tooltip: "2h" },
+                { color: "emerald", tooltip: "2h" },
+                { color: "emerald", tooltip: "2h" },
+                { color: "emerald", tooltip: "2h" },
+              ]}
             />
           );
         },
