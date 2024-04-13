c505c424831bc047b1004deb50957ef0876df220
Aldiyar <ldr.stlt@gmail.com>
2024-04-14 00:11:30 +0500

    fix: ui

--- components/chat-message.tsx
+++ components/chat-message.tsx
@@ -1,10 +1,27 @@
+import { cn } from "@/lib/utils";
+import { BotIcon, User2Icon } from "lucide-react";
+
 export default function ChatMessage({ message }: { message: any }) {
   return (
-    <div
-      key={message.id}
-      className={message.role === "user" ? "bg-slate-200" : "bg-red-200"}
-    >
-      {message.display}
+    <div className="flex gap-2">
+      {message.role === "user" ? (
+        <div className="p-2 border border-border rounded-lg size-10 flex items-center justify-center my-2">
+          <User2Icon className="size-5 text-muted-foreground" />
+        </div>
+      ) : (
+        <div className="p-2 border border-border rounded-lg size-10 flex items-center justify-center my-2">
+          <BotIcon className="size-5 text-muted-foreground" />
+        </div>
+      )}
+      <div
+        key={message.id}
+        className={cn(
+          "border border-border rounded-lg my-2 p-2",
+          message.role === "user" ? "bg-secondary" : "bg-slate-200"
+        )}
+      >
+        {message.display}
+      </div>
     </div>
   );
 }
--- components/chat.tsx
+++ components/chat.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import type { AI } from "@/app/action";
+import ChatMessage from "@/components/chat-message";
+import { Input } from "@/components/ui/input";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 import { useActions, useUIState } from "ai/rsc";
 import { useState } from "react";
 
@@ -10,18 +14,18 @@ export default function Chat() {
   const { submitUserMessage } = useActions<typeof AI>();
 
   return (
-    <div>
-      {
-        // View messages in UI state
-        messages.map((message) => (
-          <div
-            key={message.id}
-            className={message.role === "user" ? "bg-slate-200" : "bg-red-200"}
-          >
-            {message.display}
-          </div>
-        ))
-      }
+    <div className="w-xl p-2 border-border rounded-md border flex-1 flex flex-col shadow-md mx-auto">
+      <ScrollArea className="flex-1">
+        {
+          // View messages in UI state
+          messages.map((message) => (
+            <>
+              <ChatMessage message={message} key={message.id} />
+              <Separator />
+            </>
+          ))
+        }
+      </ScrollArea>
 
       <form
         onSubmit={async (e) => {
@@ -48,7 +52,7 @@ export default function Chat() {
           setInputValue("");
         }}
       >
-        <input
+        <Input
           placeholder="Send a message..."
           value={inputValue}
           onChange={(event) => {
