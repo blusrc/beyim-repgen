b617754b53e05a0b21d43dc02804183c8802196b
Miras <91594617+laqube@users.noreply.github.com>
2024-04-14 09:47:24 +0500

    -add donut

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { DatePickerWithRange } from "@/components/date-range-picker";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import useDateRangeStore from "@/lib/stores/dashboard";
-import { AreaChart, LineChart } from "@tremor/react";
+import { AreaChart, DonutChart, LineChart } from "@tremor/react";
 
 export default function DashboardPage() {
   return (
@@ -41,6 +41,43 @@ export default function DashboardPage() {
             categories={["math", "phys"]}
           />
         </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Group Performance Donut</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <DonutChart
+            data={[
+              { name: "Excellent", value: 30 },
+              { name: "Good", value: 40 },
+              { name: "Satisfactory", value: 20 },
+              { name: "Bad", value: 10 },
+            ]}
+            variant="donut"
+            colors={["green", "yellow", "orange", "red"]}
+            onValueChange={(v) => console.log(v)}
+            label="10B"
+          />
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Group Performance Per student</CardTitle>
+        </CardHeader>
+        <CardContent></CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Individual Performance over modules</CardTitle>
+        </CardHeader>
+        <CardContent></CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Individual activity over modules</CardTitle>
+        </CardHeader>
+        <CardContent></CardContent>
       </Card>
     </main>
   );
